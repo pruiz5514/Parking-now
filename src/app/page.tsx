7941c87f0f4befad5ca7264ef93b55ee3f1c2b57
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-05 20:18:30 -0500

    the basic structure of redux is created with a brief example of use.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
+import { increment } from "app/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "app/redux/hooks";
 import Image from "next/image";
 
 export default function Home() {
+  const count = useAppSelector(state => state.counterReducer.counter);
+  const dispatch = useAppDispatch();
   return (
     <main>
+      <h1>total: {count}</h1>
+      <button onClick={()=> dispatch(increment())}>Increment</button>
     </main>
   );
 }
---
+++ src/redux/provider.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import {Provider} from 'react-redux'
+import { store } from './store'
+
+interface Props {children: React.ReactNode}
+
+export function Providers ({children}:Props) {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
