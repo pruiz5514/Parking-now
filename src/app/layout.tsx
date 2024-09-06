72905a9d0607de2aac2b960a7bcdb4aea50f7488
Daniela Londono <daniela0393h@gmail.com>
2024-09-06 12:31:32 -0500

    advance in the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "app/redux/provider";
-import Header from "app/components/Header/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <Header/>
           {children}
         </Providers>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,17 @@
 import Footer from "app/components/Footer/Footer";
 import { increment } from "app/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "app/redux/hooks";
-import Image from "next/image";
+import Header from "app/components/Header/Header";
 
 export default function Home() {
   const count = useAppSelector(state => state.counterReducer.counter);
   const dispatch = useAppDispatch();
   return (
     <main>
-      {/* <Footer></Footer> */}
+      <Header/>
       <h1>total: {count}</h1>
       <button onClick={()=> dispatch(increment())}>Increment</button>
+      <Footer></Footer>
     </main>
   );
 }
