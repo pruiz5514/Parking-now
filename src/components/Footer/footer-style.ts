8f8c32a26f6da87aeeba95a60d86875c7054a42c
Daniela Londono <daniela0393h@gmail.com>
2024-09-07 13:04:32 -0500

    responsive footer completion

--- src/components/Footer/footer-style.ts
+++ src/components/Footer/footer-style.ts
@@ -1,15 +1,23 @@
 import styled from "styled-components"
+import { FaFacebook, FaWhatsapp, FaInstagram } from "react-icons/fa";
 
-export const FooterStyle = styled.footer`
-    height: 100px;
+
+export const FooterStyled = styled.footer`
+    height: 140px;
     background-color: var(--blue-dark);
     display: flex;
+    gap: 10px;
+    flex-direction: column;
     align-items: center;
     padding: 0 16px;
-    justify-content: space-between;
+    justify-content: center;
+
+    @media (min-width: 576px){
+        flex-direction: row;
+        justify-content: space-between;
+    }
 `
 export const  ContainerLogo = styled.div`
-    border: 1px solid red;
     display: flex;
     gap: 20px;
     align-items: center;
@@ -22,11 +30,30 @@ export const Title = styled.p`
     border-left: 2px solid var(--white);
 `
 
-export const  ContainerSocialMedia = styled.div`
-    border-radius: 1px solid red;
+export const  ContainerInfo = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 10px;
+`
+export const TitleCoppyRight = styled.p`
+    font-size: 16px;
+    color: var(--white);
 `
 
-export const Links = styled.link `
-font-size: 24px;
-font-weight: bold;
+export const  ContainerSocialMedia = styled.div`
+    border-radius: 1px solid red;
+    display: flex;
+    gap: 10px;
 `
+
+const commonIconStyles = `
+color: var(--white);
+font-size: 25px;
+`
+
+export const FacebookStyled = styled(FaFacebook)`${commonIconStyles}`
+
+export const WhatsappStyled = styled(FaWhatsapp)`${commonIconStyles}`
+
+export const InstagramStyled = styled(FaInstagram)`${commonIconStyles}`
