75961a1072aa1ee1a6a3960eb39d16f2dcae437a
Karina P <karina_pineda97@hotmail.com>
2024-09-08 07:26:34 -0500

    delete imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import Footer from "app/components/Footer/Footer";
 import { increment } from "app/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "app/redux/hooks";
 import Header from "app/components/Header/Header";
-import TextArea from "app/components/UI/TextArea/TextArea";
-import Input from "app/components/UI/Input/Input";
-import { MdOutlinePersonOutline } from "react-icons/md";
 
 export default function Home() {
   const count = useAppSelector(state => state.counterReducer.counter);
