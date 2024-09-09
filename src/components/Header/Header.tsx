9017e1b2fdc0dbaf402e6e165a9a5cdf822138e1
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-09 08:54:55 -0500

    feat: Mobile view is created for the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,32 @@
 "use client"
 import Image from "next/image";
 import Button from "../UI/Button/Button";
-import { PropsWithChildren } from "react";
-import { HeaderNav, HeaderSection, HeaderStyle, HeaderUl } from "./Header-style";
+import { PropsWithChildren, useState } from "react";
+import { BurgerContainer, CloseHeaderButton, HeaderAside, HeaderNav, HeaderSection, HeaderStyle, HeaderUl, HeaderUlAside } from "./Header-style";
 import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 
 
 const Header = ({ children  }: PropsWithChildren) => {
+    const [isOpen,setIsOpen] = useState(false)
+
     return(
         <HeaderStyle>
             <HeaderSection>
                 <Link href="/"><Image src="/img/LOGO1.png" alt="logo-principal" width={140} height={140}/></Link> 
             </HeaderSection>
+
             <HeaderNav>
+                <BurgerContainer>
+                    <RxHamburgerMenu />
+                </BurgerContainer>
+                <HeaderAside>
+                    <CloseHeaderButton><IoClose/></CloseHeaderButton>
+                    <HeaderUlAside>
+                        {children}
+                    </HeaderUlAside>
+                </HeaderAside>
                 <HeaderUl>
                     {children}
                 </HeaderUl>
