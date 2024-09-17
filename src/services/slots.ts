2a4700974b2ab31656e421aedccdf04f70ab3db8
Daniela Londono <daniela0393h@gmail.com>
2024-09-17 16:23:33 -0500

    form register and slots

--- src/services/slots.ts
+++ src/services/slots.ts
@@ -1,5 +1,11 @@
 export async function getSlots() {
-    const response = await fetch("https://backend-parkingnow.onrender.com/api/slots/available");
+    const response = await fetch("https://backend-parkingnow.onrender.com/api/slots/available", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwicm9sZV9pZCI6MiwiaWF0IjoxNzI2NTg3NjE0LCJleHAiOjE3MjY2NzQwMTR9.zCJ-64p-JTC3NByDPEWE8mMO8620bfm-bAfI4N4L5RU'
+        },
+    });
     const data = await response.json();
 
     if (!response.ok) {
