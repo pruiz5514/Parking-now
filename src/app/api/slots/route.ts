12dba775792158d236dd96872872796100f059d2
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-18 07:14:43 -0500

    fix: fix authorization header of slots api

--- src/app/api/slots/route.ts
+++ src/app/api/slots/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+            'Authorization': `${token}`
         }
     });
 
