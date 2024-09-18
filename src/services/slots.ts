8595903257e1c542663a5b9f7a3ab6dbd5960924
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-18 09:06:27 -0500

    feat: change url of fetching in getSlots function

--- src/services/slots.ts
+++ src/services/slots.ts
@@ -1,5 +1,5 @@
 export async function getSlots(token: string) {
-    const response = await fetch("https://backend-parkingnow-fuyg.onrender.com/api/slots/available", {
+    const response = await fetch("api/slots", {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
