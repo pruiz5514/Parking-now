efd2306ab3c97d5c46e8f01fb67130a9b2987097
Karina P <karina_pineda97@hotmail.com>
2024-09-20 08:04:38 -0500

    console-log verify data

--- src/app/parking-information/[idSlots]/parking-info/page.tsx
+++ src/app/parking-information/[idSlots]/parking-info/page.tsx
@@ -21,7 +21,7 @@ const ParkingInfo: React.FC<{ params: { idSlots: string } }> = ({ params }) => {
       try {
         if (cookieToken) {
           const slotData = await getSlotById(cookieToken, idSlots);
-          console.log(slotData);
+          console.log("answer Apigetslotbyis",slotData);
           setSlot(slotData);
         }
       } catch (e) {
