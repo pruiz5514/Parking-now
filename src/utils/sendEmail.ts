d0cd632162f375d22fe95232e234de82ff461c9c
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-19 20:20:28 -0500

    fix: useSearchParams() is be wrapped in a suspense boundary at page /forgot-password/reset-password

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,11 +4,11 @@ import { errorAlert, successAlert } from './alerts';
 
 export const sendEmail = (formRef: RefObject<HTMLFormElement>, service: string, template: string, pk: string, alert: string, token?: string): void => {
     if (formRef.current) {
-        if(token){
+        if (token) {
             const hiddenTokenInput = formRef.current.querySelector('input[name="token"]') as HTMLInputElement;
-            hiddenTokenInput.value = `http://localhost:3000/forgot-password/reset-password?code=${token}`;
+            hiddenTokenInput.value = `http://localhost:3000/forgot-password/reset-password?pn=${token}`;
         }
-        
+
         emailjs.sendForm(service, template, formRef.current, pk)
             .then(
                 () => {
