46393790fec04938948fd19e16f383cf1518fcc0
Karina P <karina_pineda97@hotmail.com>
2024-09-22 14:48:13 -0500

    api fetch and booking and banner style services

---
+++ src/app/api/booking/end-booking/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request ) {
+    const token = request.headers.get('Authorization');
+    const endbooking = await request.json()
+
+    const response = await fetch('https://backend-parkingnow-fuyg.onrender.com/api/bookings/end-booking', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`
+        },
+        body: JSON.stringify(endbooking),
+    });
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+}
+
+export async function PATCH(request: Request) {
+    const token = request.headers.get('Authorization');
+
+    const response = await fetch('https://backend-parkingnow-fuyg.onrender.com/api/bookings/end-booking/terminate', {
+        method: 'PATCH',
+        headers: {
+            'Authorization': `${token}`
+        },
+    });
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+}
