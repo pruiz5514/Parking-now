dae905f96f0a6a944fe02259d1f2532243384f98
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-23 15:24:33 -0500

    feat: comment StripePricingTable

--- src/components/StripePricingTable/StripePricingTable.tsx
+++ src/components/StripePricingTable/StripePricingTable.tsx
@@ -13,10 +13,11 @@ const StripePricingTable = () => {
     }, []);
 
     return (
-        <stripe-pricing-table
-            pricing-table-id="prctbl_1Q2GBYP9nlbropOBdJtFHKPb"
-            publishable-key="pk_test_51Q0Sj1P9nlbropOBdlLCGvxtY44VuPXE82Trsy5N04q3c5atgkUtadLLiCgRPEnRxWVIWcqtpRIkzdHau0PZSjvF007wBVHvSu"
-        />
+        <h1>Hola</h1>
+        // <stripe-pricing-table
+        //     pricing-table-id="prctbl_1Q2GBYP9nlbropOBdJtFHKPb"
+        //     publishable-key="pk_test_51Q0Sj1P9nlbropOBdlLCGvxtY44VuPXE82Trsy5N04q3c5atgkUtadLLiCgRPEnRxWVIWcqtpRIkzdHau0PZSjvF007wBVHvSu"
+        // />
     );
 };
 
