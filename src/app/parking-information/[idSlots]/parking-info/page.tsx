9b527c6d77bb0a862c182fd3f1ae007546ce8958
Karina P <karina_pineda97@hotmail.com>
2024-09-24 06:48:44 -0500

    button your reserva in prpogress

--- src/app/parking-information/[idSlots]/parking-info/page.tsx
+++ src/app/parking-information/[idSlots]/parking-info/page.tsx
@@ -10,11 +10,45 @@ import Cookies from 'js-cookie';
 import { getSlotById } from "app/services/slots";
 import { errorAlert } from "app/utils/alerts";
 import { ISlots } from "app/types/IParking";
+import { getBookinginProgressDriver } from "app/services/booking";
+import { IBookingActive } from "app/types/IBooking";
+import { useRouter } from 'next/navigation'
 
 const ParkingInfo: React.FC<{ params: { idSlots: string } }> = ({ params }) => {
   const { idSlots } = params;
   const [slot, setSlot] = useState<ISlots | null>(null);
   const cookieToken = Cookies.get("token");
+  const [BookingInProgress, setBookingInProgress] = useState<boolean>(false);
+  const [bookingActiveInfo, setBookingActiveInfo] = useState<IBookingActive>();
+  const router = useRouter();
+  useEffect(() => {
+    const checkBookingInProgress = async () => {
+        try {
+            if (cookieToken) {
+                const data = await getBookinginProgressDriver(cookieToken);
+                if (data) {
+                    setBookingInProgress(true);
+                    setBookingActiveInfo(data);
+                } else {
+                    setBookingInProgress(false);
+                }
+            } 
+        } catch (error) {
+            console.error("No hay reserva en progreso:", error);
+            setBookingInProgress(false);
+        }
+    };
+    checkBookingInProgress();
+}, [cookieToken]);
+
+const handleBookingButtonClick = () => {
+    if (BookingInProgress && bookingActiveInfo) {
+        const { slotId, bookingId } = bookingActiveInfo;
+        router.push(`/parking-information/${slotId}/reserved-parking/${bookingId}/end-reserved`);
+    } else {
+        router.push("/parkings");
+    }
+};
 
   useEffect(() => {
     const fetchSlot = async () => {
@@ -39,6 +73,7 @@ const ParkingInfo: React.FC<{ params: { idSlots: string } }> = ({ params }) => {
   return (
     <>
       <Header>
+        <li>{BookingInProgress && (<Button text="Tu reserva"onClick={handleBookingButtonClick}/>)}</li>
         <li><Link href="/parkings">Inicio</Link></li>
         <li><Link href="/register-parking">Publicar parqueadero</Link></li>
         <li><Link href="/my-parkings">Mis parqueaderos</Link></li>
