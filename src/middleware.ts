8d60a74b8c8605b75b3daeca0c0dc64484603c93
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-25 13:00:07 -0500

    feat: security is added to profiles

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,21 @@ export async function middleware(request: NextRequest) {
   else {
     try {
       const validateToken = await verifyToken(cookieToken);
+      console.log(validateToken.decode.role_id);
+
       if (!validateToken.success) {
         return NextResponse.redirect(new URL('/', request.url));
+      }
+      else if (validateToken.decode.role_id === 1) {
+        if (request.nextUrl.pathname.startsWith('/users')) {
+          return NextResponse.redirect(new URL('/', request.url));
+        }
+      }
+      else if (validateToken.decode.role_id === 2) {
+        const paths = ['/my-parkings', '/parking-information', '/payment', '/register-parking'];
+        if (paths.some(path => request.nextUrl.pathname.startsWith(path))) {
+          return NextResponse.redirect(new URL('/users', request.url));
+        }
       }
     } catch (e) {
       console.log(e)
