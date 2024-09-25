79972e01d7db4e7bcd948ebe66932005da81b4b2
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-25 16:14:57 -0500

    feat: all security page is done

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,14 +1,20 @@
+"use client"
 import Header from "app/components/Header/Header"
 import Button from "app/components/UI/Button/Button"
+import { logout } from "app/utils/logout"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const Layout = ({children}: {children:React.ReactNode})=>{
+    const router = useRouter();
     return(
         <>
             <Header>
                 <li><Link href="/parkings">Inicio</Link></li>
                 <li><Link href="/users">Usuarios</Link></li>
-                <li><Link href="/"><Button text={"Cerrar sesión"} /></Link></li>
+                <li><Link href="/register-parking">Publicar parqueadero</Link></li>
+                <li><Link href="/my-parkings">Mis parqueaderos</Link></li>
+                <li><Button text={"Cerrar sesión" } onClick={()=>{logout(); router.push("/");}}/></li>
             </Header>
             {children}
         </>
